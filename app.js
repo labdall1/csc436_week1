461d324e5fe62e2038db8527ee571b122d6db9d3
Lamees Abdallah <157441979+labdall1@users.noreply.github.com>
2024-09-25 06:48:58 +0000

    changed to prof code from week 3

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 'use strict'
+const switcher = document.querySelector('.btn'); 
 
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function(){
-    document.body.classList.toggle('lightTheme');
-    document.body.classList.toggle('darkTheme');
+switcher.addEventListener('click', function() { 
+    document.body.classList.toggle('lightTheme'); 
+    document.body.classList.toggle('darkTheme'); 
 
-    const ClassName = document.body.className;
-    if (ClassName == 'lightTheme'){
-        this.textContent = "Dark";
-
-    } else{
-        this.textContent ="Light";
-    }
+    const className = document.body.className; 
+    if(className == "lightTheme") { 
+        this.textContent = "Dark"; 
+    } else{ 
+        this.textContent = "Light"; 
+    } 
 });
